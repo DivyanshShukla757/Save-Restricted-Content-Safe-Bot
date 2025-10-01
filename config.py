af83e95f53f6d86f9ead1601e8301a9fd2ad237b
DivyanshShukla757 <divyanshshukla2239@gmail.com>
2025-10-01 08:48:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+API_ID = int(getenv("API_ID", "24712525"))
+API_HASH = getenv("API_HASH", "648708266952f0d981f5f23d97c32992")
+BOT_TOKEN = getenv("BOT_TOKEN", "8304937637:AAFeSBm59kqQZTmBOhV8MB4ug4z31RzUuQA")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6334323103").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
